58e48394b59bb876c028bdfe9304ff93aa3a290c
Eric Chen <eric_chen0503@yahoo.com>
2015-10-03 18:16:21 -0700

    First commit my nigga

---
+++ MyFirstScript.lua
@@ -0,0 +1,46 @@
+scriptId = 'com.ericc572.example'
+
+scriptTitle = "My First Script"
+scriptDetailsUrl = ""
+
+function onForegroundWindowChange (app, title)
+
+	myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+	return true
+
+end 
+
+function onPoseEdge (pose, edge)
+	myo.debug("onPoseEdge: " .. pose .. ": " .. edge)
+	if (edge == "on") then
+		if (pose == "waveOut") then
+			onWaveOut()
+		elseif (pose == "waveIn") then
+			onWaveIn()
+		elseif (pose == "fist") then
+			onFist()
+		elseif (pose == "fingersSpread") then
+			onFingersSpread()
+		end 
+	end
+end
+
+function onWaveOut()  
+    myo.debug("Send hello")
+    myo.keyboard("tab", "press")
+end
+
+function onWaveIn()  
+    myo.debug("Previous")   
+    myo.keyboard("tab","press","shift")
+end
+
+function onFist()  
+    myo.debug("Enter")  
+    myo.keyboard("return","press")
+end
+
+function onFingersSpread()  
+    myo.debug("Escape")
+    myo.keyboard("escape", "press")
+end  
